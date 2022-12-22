a9b7c81cdd456709913a40e196ac9c5e0fdad4c9
Alex <Alexecr66@gmail.com>
2022-12-22 16:29:59 -0500

    build web ,change url route

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "2f06e8d564f4ee923b0eed97464e8eac",
-"index.html": "27ece87b3255f85545a2d645c9287cf4",
-"/": "27ece87b3255f85545a2d645c9287cf4",
+"index.html": "44b02c8fb91874f4950239737f9779a3",
+"/": "44b02c8fb91874f4950239737f9779a3",
 "favicon.jpeg": "b9d85d5a4e68dff0c986804a1a220e4b",
-"main.dart.js": "b9ca6deadcbe702db3b354b0d5139a08",
+"main.dart.js": "995a689114ee82128b04751b116ed84f",
 "favicon.png": "c7332461e1ae0bd791d54a5bca7d6459",
 "icons/Icon-192.png": "c7332461e1ae0bd791d54a5bca7d6459",
 "icons/Icon-maskable-192.png": "c7332461e1ae0bd791d54a5bca7d6459",
