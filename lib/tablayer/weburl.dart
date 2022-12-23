e6be68d6e060d33d10dee255dc305536eccd76eb
Alexcr666 <alexcr66@gmail.com>
2022-12-23 13:09:11 -0500

    changed develop API_SERVER

--- lib/tablayer/weburl.dart
+++ lib/tablayer/weburl.dart
@@ -44,9 +44,11 @@ class Weburl {
   //     ? "https://prsjz0gbn3.execute-api.us-east-1.amazonaws.com/staging/"
   //     : "https://vuy3fbu93g.execute-api.us-east-1.amazonaws.com/prod/";
 
-  static var API_Server = isPROD_API
+  static var API_Server = 'http://staging.silverhomes.ai';
+  
+      /* isPROD_API
       ? "https://api.silverhomes.ai"
-      : "https://qjif09kr99.execute-api.us-east-1.amazonaws.com/dev";
+      : "https://qjif09kr99.execute-api.us-east-1.amazonaws.com/dev"; */
 
 //      : "https://25k75q7gy2.execute-api.us-east-1.amazonaws.com";
 
